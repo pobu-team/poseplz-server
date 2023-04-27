365d8a20947b831b0ba397a22ca5abcbaf967186
전해성 <seong0428@gmail.com>
2023-04-27 20:29:35 +0900

    fix: 로그인시 이상한 페이지로 redirect 되는 현상 해결 (#14)
    * refactor: Use composition instead inheritance
    
    * feat: webjars 인증 없이 접근가능하도록 변경

--- src/main/kotlin/com/poseplz/server/infrastructure/spring/PoseplzAdminOidcUserService.kt
+++ src/main/kotlin/com/poseplz/server/infrastructure/spring/PoseplzAdminOidcUserService.kt
@@ -3,11 +3,13 @@ package com.poseplz.server.infrastructure.spring
 import org.springframework.security.access.AccessDeniedException
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService
 import org.springframework.security.oauth2.core.oidc.user.OidcUser
 
-class PoseplzAdminOidcUserService : OidcUserService() {
+class PoseplzAdminOidcUserService : OAuth2UserService<OidcUserRequest, OidcUser> {
+    private val oidcUserService = OidcUserService()
     override fun loadUser(userRequest: OidcUserRequest?): OidcUser {
-        val loadedUser = super.loadUser(userRequest)
+        val loadedUser = oidcUserService.loadUser(userRequest)
         val isInternalUser = loadedUser.attributes["email"]?.let { whitelists.contains(it) } ?: false
         if (!isInternalUser) {
             throw AccessDeniedException("접근 권한이 없습니다. email: ${loadedUser.attributes["email"]}")
