7194add0a97d1357a2961e4d2a64218735308c7c
Haeseong Jeon <seong0428@gmail.com>
2023-04-29 11:39:44 +0900

    feat: 페이지 기본크기, 정렬 변경

--- src/main/kotlin/com/poseplz/server/ui/admin/pose/AdminPoseController.kt
+++ src/main/kotlin/com/poseplz/server/ui/admin/pose/AdminPoseController.kt
@@ -5,6 +5,8 @@ import com.poseplz.server.application.pose.PoseApplicationService
 import com.poseplz.server.domain.pose.PoseService
 import com.poseplz.server.domain.tag.TagService
 import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
+import org.springframework.data.web.PageableDefault
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
@@ -22,7 +24,7 @@ class AdminPoseController(
 ) {
     @GetMapping
     fun list(
-        pageable: Pageable,
+        @PageableDefault(size = 10000, sort = ["poseId"], direction = Sort.Direction.DESC) pageable: Pageable,
         model: Model,
     ): String {
         model.addAttribute("poses", poseService.findAll(pageable))
