8e9cb3e776521e22617e41cece320e387c2319e0
전해성 <seong0428@gmail.com>
2023-07-02 20:01:50 +0900

    feat: 태그 조회 조건 기본값 추가 (#41)

--- src/main/kotlin/com/poseplz/server/ui/admin/tag/AdminTagController.kt
+++ src/main/kotlin/com/poseplz/server/ui/admin/tag/AdminTagController.kt
@@ -7,6 +7,8 @@ import com.poseplz.server.domain.tag.TagType
 import com.poseplz.server.domain.tag.TagUpdateVo
 import jakarta.validation.Valid
 import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
+import org.springframework.data.web.PageableDefault
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
@@ -26,7 +28,7 @@ class AdminTagController(
      */
     @GetMapping
     fun list(
-        pageable: Pageable,
+        @PageableDefault(size = 1000, sort = ["tagId"], direction = Sort.Direction.DESC) pageable: Pageable,
         model: Model,
     ): String {
         model.addAttribute("tags", tagApplicationService.findWithCount(null, pageable).content)
