a3798aadafabb380903565e47a433cc855c343d9
전해성 <seong0428@gmail.com>
2023-08-25 15:11:00 +0900

    feat: pose 목록 응답에 file 필드 추가 (#7)

--- src/main/kotlin/com/poseplz/server/ui/api/pose/PoseSimpleResponse.kt
+++ src/main/kotlin/com/poseplz/server/ui/api/pose/PoseSimpleResponse.kt
@@ -1,8 +1,11 @@
 package com.poseplz.server.ui.api.pose
 
+import com.poseplz.server.ui.api.file.FileResponse
+
 data class PoseSimpleResponse(
     val poseId: String,
     val thumbnailImageUrl: String,
+    val file: FileResponse,
     val poseCount: Int,
     val archived: Boolean,
 )
