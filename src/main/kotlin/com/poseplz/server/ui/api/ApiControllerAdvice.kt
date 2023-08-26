c756badb7624a2bba4c4f1636db74bc29fe6fd6a
Haeseong Jeon <seong0428@gmail.com>
2023-08-26 17:24:31 +0900

    feat: 업로드 파일 크기 설정 추가

--- src/main/kotlin/com/poseplz/server/ui/api/ApiControllerAdvice.kt
+++ src/main/kotlin/com/poseplz/server/ui/api/ApiControllerAdvice.kt
@@ -6,9 +6,21 @@ import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.multipart.MaxUploadSizeExceededException
 
 @RestControllerAdvice
 class ApiControllerAdvice {
+
+    @ExceptionHandler(MaxUploadSizeExceededException::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun handleMaxUploadSizeExceededException(e: MaxUploadSizeExceededException): ApiResponse<Unit> {
+        log.error("Max upload size exceeded.", e)
+        return ApiResponse.failure(
+            resultCode = ResultCode.MAX_UPLOAD_SIZE_EXCEEDED,
+        )
+    }
+
+
     @ExceptionHandler(Exception::class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     fun handleException(e: Exception): ApiResponse<Unit> {
