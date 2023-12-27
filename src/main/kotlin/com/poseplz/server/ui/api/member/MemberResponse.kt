497127798a561c828ca9af0d7d5d120ef7cdcda1
전해성 <seong0428@gmail.com>
2023-12-27 09:42:50 +0900

    feat: 회원 정보 조회 API 추가 (#28)

--- src/main/kotlin/com/poseplz/server/ui/api/member/MemberResponse.kt
+++ src/main/kotlin/com/poseplz/server/ui/api/member/MemberResponse.kt
@@ -5,11 +5,15 @@ import com.poseplz.server.ui.api.member.MemberResponse as MemberResponse1
 
 data class MemberResponse(
     val memberId: String,
+    val name: String?,
+    val profileImageUrl: String?,
     val status: String,
 ) {
     companion object {
         fun from(memberVo: MemberVo) = MemberResponse1(
             memberId = memberVo.memberId.toString(),
+            name = memberVo.name,
+            profileImageUrl = memberVo.profileImageUrl,
             status = memberVo.status.name,
         )
     }
